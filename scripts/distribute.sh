cfb218cb761d37954acf653fe7172c6f5de19de9
Oliver Beckstein <oliver.beckstein@bioch.ox.ac.uk>
2010-09-27 14:08:03 +0100

    updated distribution script

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 PACKAGE=RecSQL
+EPYDOC_DIRS="recsql"
+PDF=RecSQL.pdf
 
-SERVERDIR=/sansom/public_html/sbcb/oliver
+SERVERDIR=/sansom/public_html/html/sbcb/oliver
 PACKAGES=$SERVERDIR/download/Python
 DOCS=$SERVERDIR/software/$PACKAGE
+
+COPY=1
 
 usage="usage: $0 [OPTIONS] [cmd1 cmd2 ...]
 
@@ -16,6 +20,7 @@ default commands: 'distribution docs'
 cmd           description
 -----         -----------------
 distribution  make sdist and egg, copy to $PACKAGES
+dist
 docs          'make_epydoc make_sphinx'
 make_epydoc   source code docs, copy to $DOCS/epydoc
 make_sphinx   documentation, copy to $DOCS/html
@@ -24,8 +29,10 @@ make_sphinx   documentation, copy to $DOCS/html
 Options
 
 -h           help
--n           do not copy
+-c           publish on server (copy) [$COPY]
+-n           do not copy              [$((1-COPY))]
 -s DIR       server dir [${SERVERDIR}]
+-p VERSION   python version, eg 2.5
 "
 
 function die () {
@@ -40,49 +47,77 @@ RSYNC () {
 }
 
 distribution () {
-  python setup.py sdist \
-      && python setup.py bdist_egg \
+  $PYTHON setup.py sdist \
+      && $PYTHON setup.py bdist_egg \
       && RSYNC -v --checksum dist/* $PACKAGES \
       || die "Failed distribution"
+}
+
+dist () {
+    distribution
 }
 
 make_epydocs() {
   epydoc -v -o doc/epydoc --html --name=$PACKAGE \
          --url=http://sbcb.bioch.ox.ac.uk/oliver/software/$PACKAGE/ \
-         recsql  \
+         ${EPYDOC_DIRS}  \
       || die "Failed making epydoc"
   RSYNC -vrP --delete doc/epydoc $DOCS
 }
 
 make_sphinx () {
-  (cd doc/sphinx && make html) || die "Failed making sphinx docs"
+  (cd doc/sphinx && make clean && make html) || die "Failed making sphinx docs"
+  (cd doc; rm -rf html; (cd sphinx/build && find html -type d) | xargs mkdir -p) && \
+      (cd doc/sphinx/build && find html -type f -exec ln -v '{}' '../../{}' \;)
+  echo "Created doc/html"
   RSYNC -vrP --delete doc/sphinx/build/html $DOCS
 }
+
+make_pdf () {
+  (cd doc/sphinx && make latex && cd build/latex && make all-pdf) || die "Failed making sphinx pdf"
+  cp doc/sphinx/build/latex/$PDF doc
+  echo "Updated pdf doc/$PDF"
+}
+
+sphinx () {
+  make_sphinx
+  make_pdf
+}    
 
 docs () {
   make_epydocs 
   make_sphinx
+  make_pdf
 }
 
 
 COPY=1
-while getopts hns: OPT; do
+while getopts hns:p: OPT; do
     case "$OPT" in
 	h) echo "$usage"; exit 0;;
 	n) COPY=0;;
+	c) COPY=1;;
 	s) SERVERDIR=$OPTARG;;
+	p) PYVERSION=$OPTARG;;
 	[?]) echo "Illegal option. See -h for usage.";
 	     exit 1;;
     esac
 done
 shift $((OPTIND-1))
 
+
 PACKAGES=$SERVERDIR/download/Python
 DOCS=$SERVERDIR/software/$PACKAGE
 
+case "$PYVERSION" in
+   2.5|2.5.*)  PYTHON=python2.5;;
+   2.6|2.6.*)  PYTHON=python2.6;;
+   2.[0-4])    die "pyversion $PYVERSION not supported";;
+   *)          PYTHON=python;;
+esac
 
 commands="$@"
-[ -n "$commands" ] || commands="distribution docs"
+[ -n "$commands" ] || commands="make_sphinx distribution"
 
 for cmd in $commands; do
     eval "$cmd"
