c953af1ebe93a28a4c9a6af6791c81f8d50bb957
Oliver Beckstein <orbeckst@gmail.com>
2012-05-01 00:06:30 -0700

    LaTeX export: quote some special characters such as underscores

--- recsql/export.py
+++ recsql/export.py
@@ -28,6 +28,18 @@ def rec2csv(r, filename):
     #print "Wrote CSV table %r" % filename
     return filename
 
+def latex_quote(s):
+    """Quote special characters for LaTeX.
+
+    (Incomplete, currently only deals with underscores, dollar and hash.)
+    """
+    special = {'_':r'\_', '$':r'\$', '#':r'\#'}
+    s = str(s)
+    for char,repl in special.items():
+        new = s.replace(char, repl)
+        s = new[:]
+    return s
+
 def rec2latex(r, filename, empty=""):
     """Export a recarray *r* to a LaTeX  table in *filename*"""
     with open(filename, "w") as latex:
@@ -41,10 +53,10 @@ def s_rec2latex(r, empty=""):
     def translate(x):
         if x is None or str(x).lower == "none":
             x = empty
-        return str(x)
+        return latex_quote(x)
     latex += r"\begin{tabular}{%s}" % ("".join(["c"]*len(names)),) + "\n"  # simple c columns
     latex += r"\hline"+"\n"
-    latex += " & ".join([str(x) for x in names])+r"\\"+"\n"
+    latex += " & ".join([latex_quote(x) for x in names])+r"\\"+"\n"
     latex += r"\hline"+"\n"
     for data in r:
         latex += " & ".join([translate(x) for x in data])+r"\\"+"\n"
